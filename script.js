b6a058895eaa78a59e9f4d332ae8fcb52b0a8bc2
Michelle <mpuschkarow@web.de>
2025-05-09 09:22:13 +0200

    set conditions to move character

--- script.js
+++ script.js
@@ -24,4 +24,32 @@ function draw(){
     rect(platform.x, platform.y, 150, 20)
     fill('#FFD700');
     rect(platform2.x, platform2.y, 150, 20)
+
+    player.y += jumpForce; //apply jump force to player
+    player.x += movement; //apply movement to player
+
+    if(player.y >= ground){
+        jumpForce = 0;
+
+    }else if(//all obj are centered; we need x + half width / x - half width
+        player.x + 25 >= platform.x - 75 &&
+        player.x - 25 <= platform.x + 75 &&
+        player.y + 25 >= platform.y - 10 &&
+        player.y + 25 <= platform.y + 10 
+    ){
+        jumpForce = 0;
+        player.y = platform.y - 35;
+    }else{
+         jumpForce += 0.5; //gravity effect
+    }
+     if(keyIsDown(32) && (player.y >= ground || player.y == platform.y - 35)){ //space key
+        jumpForce = -12;
+     }
+      if(keyIsDown(RIGHT_ARROW)){ //right key
+        movement = +3;
+     } else if(keyIsDown(LEFT_ARROW)){ //left key
+        movement = -3;
+     } else{
+        movement = 0;
+     }
 }
